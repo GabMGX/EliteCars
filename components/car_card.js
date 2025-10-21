3917812260dc73524d397e86d342e828ade6c1a9
debhoora <debhorathamyris15@gmail.com>
2025-10-21 20:25:18 -0300

    Atualização car-card css

--- components/car_card.js
+++ components/car_card.js
@@ -3,6 +3,8 @@ class CarCard extends HTMLElement {
         super();
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.innerHTML = ``;
+        this.image = this.shadowRoot.getElementById("imagem");
+        this.image.src = "https://blog.deltafiat.com.br/wp-content/uploads/2023/08/carro-completo.jpeg";
     }
 }
 
